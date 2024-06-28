52e8b637e0e6fd2b7fc51296698cc48f660662fe
Rukovitsyn Kirill <kirillms29@gmail.com>
2024-06-28 14:19:53 +0300

    create repo

---
+++ src/main/java/ru/kerchik/loggingstarter/LoggingConfiguration.java
@@ -0,0 +1,7 @@
+package ru.kerchik.loggingstarter;
+
+public class LoggingConfiguration {
+    public static void test(String message) {
+        System.out.println("Вызвано из стартера " + message);
+    }
+}
